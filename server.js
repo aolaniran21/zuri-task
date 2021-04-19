f7041c1e5405c336bb44fe721bf6854f505bc642
Ayodeji Olaniran <ayodejiolaniran19@gmail.com>
2021-04-20 00:04:14 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const path = require('path');
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'post.json'
+);
+
+const server = (res, req) => {
+    fetch("http://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json()) 
+        // .then((response) => { 
+        //     console.log(response.json());
+        //     const jsonContent = JSON.stringify(response.json());
+        //     fs.writeFile(p, jsonContent, (err) => {
+        //         console.log(err);
+        //     });
+        // })
+        .then(json => {
+            
+            const jsonContent = JSON.stringify(json);
+            fs.writeFile(p, jsonContent, (err) => {
+                console.log(err);
+            });
+
+        })
+        .catch((err) => console.log(err));
+
+    req.end('success')
+
+}
+
+http.createServer(server).listen(4000, () => console.log('running'));
